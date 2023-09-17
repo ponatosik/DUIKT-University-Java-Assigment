070d9aa7f719a61a5b7952785275945e7ad6462b
ponatosik <ponatsoik@gmail.com>
2023-09-17 16:44:44 +0300

    feat: Add DVD Item

---
+++ src/main/java/com/bondarenko/universityAssigment/library/DVD.java
@@ -0,0 +1,20 @@
+package com.bondarenko.universityAssigment.library;
+
+public class DVD extends Item {
+    /** DVD video duration (in minutes) */
+    private final int duration;
+
+    /** Initializes newly created DVD object with
+     * @param title DVD title
+     * @param duration DVD duration (in minutes) */
+    public DVD (String title, int duration)
+    {
+        super(title);
+        this.duration = duration;
+    }
+
+    /** @return DVD video duration (in minutes) */
+    public int getDuration() {
+        return duration;
+    }
+}
