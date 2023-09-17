4ab06598c55fd970084cba512b28f205b9014dd3
ponatosik <ponatsoik@gmail.com>
2023-09-17 21:39:17 +0300

    test: add tests for borrowing items

--- src/test/java/com/bondarenko/universityAssigment/library/LibraryTest.java
+++ src/test/java/com/bondarenko/universityAssigment/library/LibraryTest.java
@@ -1,6 +1,7 @@
 package com.bondarenko.universityAssigment.library;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,17 +11,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class LibraryTest {
     Library library;
     List<Item> libraryItems;
+    List<Patron> libraryPatrons;
     Book uniqueBook;
 
     @BeforeEach
     void setUp() {
-        uniqueBook = new Book("1984 (Nineteen Eighty-Four)", " George Orwell", "978-0241453513", 1949);
+        uniqueBook = new Book("1984 (Nineteen Eighty-Four)", "George Orwell", "978-0241453513", 1949);
         libraryItems = List.of(
                 new Book("Harry Potter and the Sorcerer's Stone", " J.K. Rowling", "978-1338878929", 1998),
                 new Book("Java For Kids", " J.K. Rowling", "978-1981597314", 2017),
-                new Book("The Lord Of The Rings", " J.R.R. Tolkien", "978-0544003415", 1954)
+                new Book("The Lord Of The Rings", " J.R.R. Tolkien", "978-0544003415", 1954),
+                new DVD("The Lord of the Rings: The Fellowship of the Ring", 178),
+                new DVD("Harry Potter and the Deathly Hallows: Part 1", 146)
         );
-        library = new Library(libraryItems);
+        libraryPatrons = List.of(
+                new Patron("John"),
+                new Patron("Walter")
+        );
+        library = new Library(libraryItems, libraryPatrons);
     }
 
     @Test
@@ -78,5 +86,93 @@ class LibraryTest {
         var actual = library.removeById(bookId);
 
         assertFalse(actual);
+    }
+
+    @Test
+    void RegisterPatron_ValidPatron_ShouldNotThrowError() {
+        var patron = new Patron("Olexandr");
+
+        assertDoesNotThrow(() -> library.registerPatron(patron));
+    }
+
+    @Test
+    void LendItem_ValidPatronAndItem_ShouldMakeItemBorrowed() throws UnknownPatronException, ItemCannotBeBorrowedException {
+        var patron = new Patron("Olexandr");
+        library.registerPatron(patron);
+        library.add(uniqueBook);
+
+        library.lendItem(patron, uniqueBook);
+
+        assertTrue(uniqueBook.isBorrowed());
+    }
+
+    @Test
+    void LendItem_UnregisteredPatron_ShouldThrowUnknownPatronException() {
+        var patron = new Patron("Unregistered");
+        library.add(uniqueBook);
+
+        assertThrows(UnknownPatronException.class, () -> library.lendItem(patron, uniqueBook));
+    }
+
+    @Test
+    void LendItem_BorrowedItem_ShouldThrowItemCannotBeBorrowedException() throws UnknownPatronException, ItemCannotBeBorrowedException {
+        var patron1 = new Patron("Patron1");
+        var patron2 = new Patron("Patron2");
+        library.registerPatron(patron1);
+        library.registerPatron(patron2);
+        library.add(uniqueBook);
+
+        library.lendItem(patron1, uniqueBook);
+
+        assertThrows(ItemCannotBeBorrowedException.class, () -> library.lendItem(patron2, uniqueBook));
+    }
+
+    @Test
+    void ReturnItem_ValidPatronAndItem_ShouldMakeItemNotBorrowed() throws UnknownPatronException, ItemCannotBeBorrowedException {
+        var patron = new Patron("Olexandr");
+        library.registerPatron(patron);
+        library.add(uniqueBook);
+
+        library.lendItem(patron, uniqueBook);
+        library.returnItem(patron, uniqueBook);
+
+        assertFalse(uniqueBook.isBorrowed());
+    }
+
+    @Test
+    void ListAvailable_ValidPatronAndItem_ShouldReturnNotBorrowedItems() throws ItemCannotBeBorrowedException {
+        var borrowedItem = uniqueBook;
+        uniqueBook.borrowItem();
+        library.add(borrowedItem);
+        var expected = library.getItems().stream().filter(Predicate.not(Item::isBorrowed)).toArray();
+
+        var actual = library.listAvailable();
+
+        assertArrayEquals(expected, actual.toArray());
+    }
+
+    @Test
+    void ListBorrowed_ValidPatronAndItem_ShouldReturnBorrowedItems() throws ItemCannotBeBorrowedException {
+        var borrowedItem = uniqueBook;
+        uniqueBook.borrowItem();
+        library.add(borrowedItem);
+        var expected = library.getItems().stream().filter(Item::isBorrowed).toArray();
+
+        var actual = library.listBorrowed();
+
+        assertArrayEquals(expected, actual.toArray());
+    }
+
+    @Test
+    void GetItemBorrower_BorrowedItem_ReturnsPatronThatBorrowedItem() throws UnknownPatronException, ItemCannotBeBorrowedException {
+        var actual = new Patron("Olexandr");
+        library.registerPatron(actual);
+        library.add(uniqueBook);
+        library.lendItem(actual, uniqueBook);
+
+        var expected = library.getItemBorrower(uniqueBook);
+
+        assertTrue(expected.isPresent());
+        assertEquals(expected.get(), actual);
     }
 }
