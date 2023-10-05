2079915e73152b3da2d6fd602e37a73fe484eed8
ponatosik <ponatosik@gmail.com>
2023-10-05 16:29:04 +0300

    refactor(task4): extract JavaDecoder methods

---
+++ src/main/java/com/bondarenko/universityAssigment/lab4/Decoder.java
@@ -0,0 +1,6 @@
+package com.bondarenko.universityAssigment.lab4;
+
+public interface Decoder {
+    String decode(String word);
+    boolean isEncoded(String word);
+}
--- src/main/java/com/bondarenko/universityAssigment/lab4/JavaDecoder.java
+++ src/main/java/com/bondarenko/universityAssigment/lab4/JavaDecoder.java
@@ -1,11 +1,10 @@
 package com.bondarenko.universityAssigment.lab4;
 
+import com.bondarenko.universityAssigment.lab4.DecodeMethods.CaesarCipherDecoder;
+import com.bondarenko.universityAssigment.lab4.DecodeMethods.RankCipherDecoder;
+
 import java.util.List;
-import java.util.Map;
 import java.util.function.Predicate;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -16,69 +15,28 @@ public class JavaDecoder {
     private final static List<Character> VOWELS = List.of('a', 'e', 'i', 'o', 'u', 'y');
     private final static List<Character> CONSONANTS = getConsonants();
 
-    private final static Map<Character, Character> VOWEL_CODES = getVowelCodes();
-    private final static Map<Character, Character> CONSONANT_CODES = getConsonantCodes();
+    private final static Decoder vowelsDecoder = new RankCipherDecoder(VOWELS);
+    private final static Decoder consonantDecoder = new CaesarCipherDecoder(CONSONANTS);
 
-    public static String decodeVowels (String word) {
-        return decode(word, VOWEL_CODES);
+    public static String decode(String word) {
+        if(vowelsDecoder.isEncoded(word))
+        {
+            return vowelsDecoder.decode(word);
+        }
+        else if(consonantDecoder.isEncoded(word))
+        {
+            return consonantDecoder.decode(word);
+        }
+        return word;
     }
 
-    public static String decodeConsonants (String word) {
-        return decode(word, CONSONANT_CODES);
-    }
-
-    public static boolean isVowelsEncoded (String word) {
-        return testDecoder(word, VOWEL_CODES);
-    }
-
-    public static boolean isConsonantsEncoded (String word) {
-        return testDecoder(word, CONSONANT_CODES);
-    }
-
-    public static boolean testDecoder(String word, Map<Character, Character> codes) {
-        Pattern pattern = getRegex(codes);
-        Matcher matcher = pattern.matcher(word);
-        return matcher.matches();
-    }
-
-    private static String decode (String word, Map<Character, Character> codes) {
-        StringBuilder builder = new StringBuilder();
-        word.chars().forEach(charCode -> {
-            char character = (char) charCode;
-            builder.append(codes.getOrDefault(character, character));
-        });
-        return builder.toString();
+    public static boolean isEncoded(String word) {
+        return vowelsDecoder.isEncoded(word) || consonantDecoder.isEncoded(word);
     }
 
     private static List<Character> getConsonants(){
         return Stream.iterate('a', character -> (int)character <= (int)'z', character -> (char)((int) character + 1))
                 .filter(Predicate.not(VOWELS::contains))
                 .toList();
-    }
-
-    private static Map<Character, Character> getVowelCodes() {
-        return VOWELS.stream().collect(Collectors.toMap(
-                vowel -> digitToChar(VOWELS.indexOf(vowel) + 1),
-                vowel -> vowel
-        ));
-    }
-
-    private static Map<Character, Character> getConsonantCodes() {
-        return CONSONANTS.stream().collect(Collectors.toMap(
-                consonant -> CONSONANTS.get((CONSONANTS.indexOf(consonant) + 1) % CONSONANTS.size() ),
-                consonant -> consonant
-        ));
-    }
-
-    private static Pattern getRegex (Map<Character, Character> codes) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(".*[");
-        codes.keySet().forEach(builder::append);
-        builder.append("]+.*");
-        return Pattern.compile(builder.toString());
-    }
-
-    private static char digitToChar(int digit) {
-        return (char)(digit + '0');
     }
 }
