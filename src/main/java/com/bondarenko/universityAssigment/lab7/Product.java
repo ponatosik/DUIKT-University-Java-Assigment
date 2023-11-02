146bafc3cb1e9a60759a25b20fce0edc6e247228
ponatosik <ponatsoik@gmail.com>
2023-11-02 21:29:13 +0200

    feat(task7): implement Product comparators

--- src/main/java/com/bondarenko/universityAssigment/lab7/Product.java
+++ src/main/java/com/bondarenko/universityAssigment/lab7/Product.java
@@ -2,14 +2,28 @@ package com.bondarenko.universityAssigment.lab7;
 
 import lombok.*;
 
+import java.util.Comparator;
+
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
-public class Product {
+public class Product implements Comparable<Product>{
     private final int id;
     private String name;
     private double price;
     private int stock;
-}
 
+    public static Comparator<Product> nameComparator(){
+        return (product1, product2) -> product1.name.compareTo(product2.name);
+    }
+
+    public static Comparator<Product> stockComparator(){
+        return (product1, product2) -> product1.stock - product2.stock;
+    }
+
+    @Override
+    public int compareTo(Product other) {
+        return (int)(other.price - this.price);
+    }
+}
 
